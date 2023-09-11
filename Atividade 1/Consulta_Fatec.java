4cd44ac0faf83e50628f3cdad3dccf552f7fa49a
Camila Sato <141842701+japacami@users.noreply.github.com>
2023-09-10 21:18:28 -0300

    Add Atividade 1.
    Compreender o funcionamento do objeto (atributos, métodos, classe e variável de instância) dentro de um programa.

---
+++ Atividade 1/Consulta_Fatec.java
@@ -0,0 +1,12 @@
+package Atividade1;
+
+public class Consulta_Fatec {
+	
+	public static void main (String[] args) {
+		//variável de instância
+		Fatec f = new Fatec();
+		f.Retorna_unidade("Baixada Santista");
+		f.Retorna_curso("Administração de Sistemas - manhã");
+	}
+
+}
---
+++ Atividade 1/Fatec.java
@@ -0,0 +1,17 @@
+package Atividade1;
+
+public class Fatec {
+	//atributos
+	String unidade;
+	String curso;
+	
+	//métodos
+	public void Retorna_unidade(String u) {
+		System.out.println("Unidade da Fatec:\t" + u);
+	}
+	
+	public void Retorna_curso(String c) {
+		System.out.println("Curso da Fatec:\t" + c);
+	}
+
+}
