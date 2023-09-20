26220036c16f33ea8489ac5497b18554f856dfcb
Camila Sato <141842701+japacami@users.noreply.github.com>
2023-09-20 08:16:45 -0300

    Add Atividade 10.
    Continuação em relação à herança.

---
+++ Atividade 10/Fatec/Disciplinas_CEETEPs.java
@@ -0,0 +1,14 @@
+package Fatec;
+
+public class Disciplinas_CEETEPs extends Faculdade_CEETEPs {
+	private String nome_disciplina;
+	
+	public void setDisciplina(String nome_disciplina) {
+		this.nome_disciplina = nome_disciplina;
+	}
+	
+	public String getDisciplina() {
+		return nome_disciplina;
+	}
+
+}
